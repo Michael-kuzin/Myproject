417dbb41b1ea408c8b1429712701e4e898a352d1
Michael-kuzin <kuzin453@gmail.com>
2020-07-20 16:29:38 +0300

    modified index and package.json

--- main/index.js
+++ main/index.js
@@ -7,6 +7,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(bodyParser.json());
 
+const arr = [];
 // app.use(function (req, res) {
 //     res.setHeader('Content-Type', 'text/plain');
 //     res.write('you posted:\n');
@@ -14,12 +15,60 @@ app.use(bodyParser.json());
 // });
 
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.json(arr);
 });
 
-app.post('/test',function (req,res) {
-    res.write('you posted:\n');
-    res.end(JSON.stringify(req.body, null, 2))
+app.get('/:id',function(req,res) {
+  console.log("get obj by id");
+    let result = arr.filter(item => item.id === JSON.parse(req.params.id)) ? arr.filter(item => item.id === JSON.parse(req.params.id))[0] : null;
+    res.send(result)
+});
+
+app.post('/',function (req,res) {
+    // res.write('you posted:\n');
+    // res.end(JSON.stringify(req.body, null, 2))
+      let reqObj = req.body;
+      arr.push(reqObj);
+      res.json(reqObj);
+});
+
+app.put('/:id',function(req,res) {
+  let reqObj = req.body;
+  let reqID = req.params.id;
+      arr.forEach((item, i) => {
+          if(item.id === reqID) {
+            arr.splice(i,1);
+            arr.push(reqObj);
+          }
+      });
+    res.json(reqObj);
+});
+
+
+app.patch('/:id',function(req,res) {
+  let reqObj = req.body;
+  let reqID = req.params.id;
+  let result;
+        arr.forEach((item, i) => {
+            if(item.id == reqID) {
+             let prop = Object.keys(reqObj);
+                  prop.forEach((key, i) => {
+                    item[key] = reqObj[key];
+                  });
+              result = item;
+            };
+         });
+         console.log(result);
+    res.json(result);
+})
+
+app.delete('/:id',function(req,res) {
+  let reqID = req.params.id;
+   arr.forEach((item, i) => {
+       if(item.id === reqID) {
+         arr.splice(i,1);
+       }
+   });
 });
 
 app.listen(3000, function () {
