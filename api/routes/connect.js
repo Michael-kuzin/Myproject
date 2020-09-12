057377fbdf07e22b45c681ccec7ae207fa304eee
Michael-kuzin <kuzin453@gmail.com>
2020-09-12 01:46:32 -0900

    change some files

--- api/routes/connect.js
+++ api/routes/connect.js
@@ -6,22 +6,17 @@ const axios = require('axios');
 router.get('/', async function (req, res) {
     const result = await axios.get('http://localhost:3000/api/v1/users')
     console.log('result');
-    console.log(result);
     console.log(result.data);
-    console.log(result.data[0]);
 
-    const cars = await Promise.all(result.data[0].cars.map(carId => {
-        console.log('car');
-        console.log(carId);
-        return new Promise((resolve, reject) => {
-            axios.get('http://localhost:3000/api/v1/cars/${carId}').then(res => {
-                resolve(res);
-            })
-            .catch(err => {
-                throw new Error(err);
-            })
-        });
-    }));
+
+    const cars = await Promise.all()        
+
+    let dvadvavosem = [1,2,3,4];
+    let arr = dvadvavosem.map(elem => {
+        return new Promise((resolve,reject) => {
+            resolve(elem)    
+        }) 
+    })
 
 
 
